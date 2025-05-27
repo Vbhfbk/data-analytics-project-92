2e5b82a90d705cd871015799f7105d4d7da17dcf
Vbhfbk <inlimine2023@gmail.com>
2025-05-27 20:33:40 +0300

    Add files via upload

--- queries.sql
+++ queries.sql
@@ -80,3 +80,30 @@ left join products p
 	on s.product_id = p.product_id
 group by 1 --группируем данные по дате
 order by 1; --сортируем данные по дате по возрастанию 
+
+--данные покупателей, первая покупка которых была в ходе проведения акций
+with tab as (
+select
+	concat(c.first_name,' ',c.last_name) as customer,
+	concat(e.first_name,' ',e.last_name) as seller,
+	s.sale_date,
+    p.price, --цена товара в покупке
+    s.customer_id,
+    row_number()over (partition by concat(c.first_name,' ',c.last_name) order by s.sale_date, s.sales_id) as sales_number 
+    --порядковый номер покупки для конкретного покупателя, дополнительное ранжирование по sales_id для выявления первой покупки в рамках дня
+from sales s 
+left join customers c
+	on s.customer_id = c.customer_id
+left join products p
+	on s.product_id = p.product_id
+left join employees e
+	on s.sales_person_id = e.employee_id
+where price = 0
+)
+select
+customer, --имя и фамилия покупателя
+sale_date, --дата покупки
+seller --имя и фамилия продавца
+from tab
+where sales_number = 1
+order by customer_id; 
